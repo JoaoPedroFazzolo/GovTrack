46a10fcfde63b268167c3255e869dc3ec7becec7
JoaoPedroFazzolo <143749763+JoaoPedroFazzolo@users.noreply.github.com>
2025-03-15 22:38:47 -0300

    chore: cria o componente padrao de background do app

--- app/src/main/java/com/fiap/govtrack/ui/theme/Background.kt
+++ app/src/main/java/com/fiap/govtrack/ui/theme/Background.kt
@@ -1,2 +1,42 @@
-package com.fiap.govtrack.ui.theme
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
+@Composable
+fun GradientBackground(content: @Composable () -> Unit) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                brush = Brush.linearGradient(
+                    colors = listOf(
+                        Color(0xFF302F2F),
+                        Color(0xFF030303)
+                    ),
+                )
+            )
+            .padding(
+                top = 64.dp,
+                start = 32.dp
+            ),
+        contentAlignment = Alignment.TopCenter
+    ) {
+        content()
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun GradientBackgroundPreview() {
+    GradientBackground {
+        Box(modifier = Modifier.fillMaxSize())
+    }
+}
